d413be64b4bcf592c38e7d7cc1e8c70b291a7568
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-08-21 21:52:22 +0200

    add player

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame
 from pygame import Color
 
 from constants import *
+from player import Player
+
 
 def main():
     print("Starting asteroids!")
@@ -15,13 +17,17 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black = Color(0, 0, 0)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # draw
         screen.fill(black)
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
